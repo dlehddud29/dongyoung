1d023b1872d9940bc24009e487a4f62673a6534f
1 <1@1-PC>
2020-07-09 08:46:32 +0900

    add Scanner

--- Git_Test/src/git/Git_test.java
+++ Git_Test/src/git/Git_test.java
@@ -1,10 +1,14 @@
 package git;
 
+import java.util.Scanner;
+
 public class Git_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		int a = 0;
+		
 	}
 
 }
